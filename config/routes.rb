2153f269a4e950fcb97016fe343b19078124d54c
EC2 Default User <ec2-user@ip-172-31-45-200.ec2.internal>
2021-04-08 01:09:39 +0000

    adminのitemsルーティング、viewの雛形作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
-    resources :orders, only:[:index]
+    get "/" => "homes#top"
+    resources :items, except: [:destroy]
+
   end
 end
