e5eac5f53529210d2678a5026a44c085d7ea3335
EC2 Default User <ec2-user@ip-172-31-45-200.ec2.internal>
2021-04-08 00:27:18 +0000

    refileのインストール完了

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   }
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :orders, only:[:index]
+  end
 end
